472d9f00e0564b1690b6f426cbe3089d247a7e17
Mampson <matthew.sampson@my.jcu.edu.au>
2018-09-04 12:52:49 +1000

    Fix count names program

--- Prac_05/count_names.py
+++ Prac_05/count_names.py
@@ -8,17 +8,30 @@ by Matthew Sampson
 def main():
     words_entry = input("Enter your string: ")
     strings = words_entry.split(" ")
+    sorted_list = build_string_list(strings)
+    print_string_count(sorted_list)
+
+
+def build_string_list(strings):
+    """Build sorted list containing both all string elements and their occurrence count"""
 
     for i in range(len(strings)):
         string = strings[i]
         count = get_number_of_word(strings[i])
-        print_string_count(string, count)
+        string_counted_list = [string, count]
+
+    string_counted_list.sort()
+
+    return string_counted_list
 
 
-def print_string_count(string, number):
+
+
+def print_string_count(string_list):
     """Print string with value within list of strings"""
 
-    print("{} : {}".format(string, number))
+    print(string_list)
+    #print("{} : ".format(string_list)
 
 
 def get_number_of_word(strings):
